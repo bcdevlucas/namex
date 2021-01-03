dcc13420d4d3fa2d017021c288c2acc3e79d7522
Lucas Lopatka <lucas@Lucass-MacBook-Pro.local>
2021-01-02 22:05:33 -0800

    Tweak app startup.

--- services/auto-analyze/src/auto_analyze/__init__.py
+++ services/auto-analyze/src/auto_analyze/__init__.py
@@ -42,10 +42,53 @@ async def create_app(run_mode):
         quart_app.config.from_object(config.CONFIGURATION[run_mode])
         db.init_app(quart_app)
         ma.init_app(quart_app)
+        quart_app.db = db
     except Exception as err:
         quart_app.logger.debug(
             'Error creating application in auto-analyze service: {0}'.format(repr(err.with_traceback(None))))
         raise
+
+    @quart_app.route('/', methods=['POST'])
+    async def main():
+        """Return the outcome of this private service call."""
+        name_analysis_service = ProtectedNameAnalysisService()
+        service = name_analysis_service
+        np_svc_prep_data = service.name_processing_service
+        np_svc_prep_data.prepare_data()
+
+        json_data = await request.get_json()
+        list_dist = json_data.get('list_dist')
+        list_desc = json_data.get('list_desc')
+        list_name = json_data.get('list_name')
+        dict_substitution = json_data.get('dict_substitution')
+        dict_synonyms = json_data.get('dict_synonyms')
+        matches = json_data.get('names')[:75]
+
+        app.logger.debug('Number of matches: {0}'.format(len(matches)))
+
+        start_time = time()
+        result = await asyncio.gather(
+            *[auto_analyze(name, list_name, list_dist, list_desc, dict_substitution, dict_synonyms, np_svc_prep_data)
+              for
+              name in matches]
+        )
+        print('--- Conflict analysis for {count} matches in {time} seconds ---'.format(
+            count=len(matches),
+            time=(time() - start_time)
+        ))
+        print('--- Average match analysis time: {time} seconds / name ---'.format(
+            time=((time() - start_time) / len(matches))
+        ))
+
+        return jsonify(result=result)
+
+    @quart_app.after_request
+    def after_request(response):
+        if db is not None:
+            print('Closing AutoAnalyze service DB connections')
+            db.engine.dispose()
+
+        return response
 
     @quart_app.after_request
     def add_version(response):  # pylint: disable=unused-variable; linter not understanding framework call
